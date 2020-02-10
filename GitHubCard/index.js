722e465d03a9f7066ade81f78179e1604bb8e5b9
indiMjc <45151346+indiMjc@users.noreply.github.com>
2020-02-10 13:50:09 -0500

    better comments, removed unneeded comments and moved alternate example to bottom

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,3 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
 const cardContainer = document.querySelector('.cards');
 
 axios
@@ -16,7 +12,7 @@ axios
 axios
 	.get('https://api.github.com/users/indiMjc/followers')
 	.then(response => {
-		//stretch
+		// stretch
 		response.data.forEach(user => {
       axios.get(user.url)
         .then(response => {
@@ -26,36 +22,10 @@ axios
 	})
 	.catch(error => {
 		console.log('The data was not returned', error);
-	});
-
-// Refactored using Promise.all, async/await and IFFE (Immediately Invoked Function Expression)
-// The function is wrapped ( ... )();  This is called an IFFE.  When the compiler reads this .js file, it will invoke the function immediately
-// Before the function keyword, we tell the compiler that this is an asynchronous function with the 'async' keyword
-
-// (async function getData() {
-// 	try {
-// 		// The destructured array represents the response from each of the axios calls, the first value is from the first axios call.  We tell the compiler to wait on the values returned from these axios calls with the 'await' keyword.  Promise.all() wraps these promises into a single function call.  We could also call them individually, each with the 'await' keyword
-// 		const [myUserData, myFollowersData] = await Promise.all([
-// 			axios.get('https://api.github.com/users/indiMjc'),
-// 			axios.get('https://api.github.com/users/indiMjc/followers')
-// 		]);
-
-// 		cardContainer.appendChild(CardMaker(myUserData.data));
-
-// 		// Since we're doing axios calls inside this loop, we tell the compiler to wait on them with the 'await' keyword before the loop callback paramater
-// 		myFollowersData.data.forEach(async user => {
-// 			// We tell the compiler to wait on the response from this axios calls and declare the response as a variable 'getFollower'
-// 			const getFollower = await axios.get(user.url);
-
-// 			cardContainer.appendChild(CardMaker(getFollower.data));
-// 		});
-// 	} catch (err) {
-// 		console.log('Data was not returned', err);
-// 	}
-// })();
+  });
 
 function CardMaker(userData) {
-	//create elements
+	// create elements
 	let card = document.createElement('div');
 	let userImg = document.createElement('img');
 	let cardInfo = document.createElement('div');
@@ -68,13 +38,13 @@ function CardMaker(userData) {
 	let followingCount = document.createElement('p');
 	let bio = document.createElement('p');
 
-	//classes
+	// add classes
 	card.classList.add('card');
 	cardInfo.classList.add('card-info');
 	user.classList.add('name');
 	userName.classList.add('username');
 
-	//populate
+	// populate elements
 	userImg.src = userData.avatar_url;
 	user.textContent = userData.name;
 	userName.textContent = userData.login;
@@ -86,7 +56,7 @@ function CardMaker(userData) {
 	followingCount.textContent = `Following: ${userData.following}`;
 	bio.textContent = `Bio: ${userData.bio}`;
 
-	//structure
+	// structure/assemble the elements
 	card.appendChild(userImg);
 	card.appendChild(cardInfo);
 	cardInfo.appendChild(user);
@@ -101,53 +71,44 @@ function CardMaker(userData) {
 	return card;
 }
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+  
 
-   Skip to Step 3.
-*/
+// Alternative solution:
+  
+// Vocabulary:  Promises, Promise.all(), async/await, IFFE (Immediately Invoked Function Expression), destructuring
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+// Refactored .then().catch() example above using Promise.all, async/await and IFFE.
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+// The function is wrapped ( ... )();  This is called an IFFE.  When the compiler reads this .js file, it will invoke the function immediately.
+// Before the function keyword, we tell the compiler that this is an asynchronous function with the 'async' keyword.
+// (async function getData() {
+// 	try {
+//     // The destructured array represents the response from each of the axios calls, 
+//     // the first value is from the first axios call.  We tell the compiler to wait 
+//     // on the values returned from these Promises with the 'await' keyword.  
+//     // Promise.all() wraps these promises into a single function call.  These values 
+//     // that are returned as Promises can be accessed by referencing the variable 
+//     // names from the destructured array.  
+    
+//     // We could also invoke the Promises individually, each with the 'await' keyword, 
+//     // ex: const myUserData = await axios.get('https://api.github.com/users/indiMjc');
+// 		const [myUserData, myFollowersData] = await Promise.all([
+// 			axios.get('https://api.github.com/users/indiMjc'),
+// 			axios.get('https://api.github.com/users/indiMjc/followers')
+// 		]);
 
-const followersArray = [];
+// 		cardContainer.appendChild(CardMaker(myUserData.data));
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+//     // Since we're doing axios calls inside this loop, we tell the compiler that this is an 
+//     // asynchronous function with the 'async' keyword before the loop callback paramater
+// 		myFollowersData.data.forEach(async user => {
+//       // We tell the compiler to wait on the response from this axios call with the 'await' 
+//       // keyword and declare the response as a variable 'getFollower'
+// 			const getFollower = await axios.get(user.url);
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+// 			cardContainer.appendChild(CardMaker(getFollower.data));
+// 		});
+// 	} catch (err) {
+// 		console.log('Data was not returned', err);
+// 	}
+// })();
